dff844f73c5d05062fdf76391fb521863a3c6084
m-aljamal <mohammadjamol@gmail.com>
2021-09-10 08:40:31 +0300

    add google autocomplete

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_API_KEY } from "@env";
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
@@ -14,6 +16,26 @@ const HomeScreen = () => {
             resizeMode: "contain",
           }}
         />
+
+        <GooglePlacesAutocomplete
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          placeholder="Where From?"
+          enablePoweredByContainer={false}
+          query={{
+            key: GOOGLE_API_KEY,
+            language: "en",
+          }}
+        />
+
         <NavOptions />
       </View>
     </SafeAreaView>
